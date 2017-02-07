87ffde3963827b4dfc244ac11daadcb433de322a
VaioJun <1014gin@Gmail.com>
2017-02-07 16:59:46 -0500

    Worked on cardview

---
+++ app/src/main/java/com/example/vaiojun/dimension4/MainPage.java
@@ -0,0 +1,13 @@
+package com.example.vaiojun.dimension4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
