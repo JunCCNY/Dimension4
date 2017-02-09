b51c64499b7231b9fb5d6cde6bd18186a70a9220
JunLi@CCNY <1014gin@gmail.com>
2017-02-09 14:46:15 -0500

    login added

--- app/src/main/java/com/example/vaiojun/dimension4/Activity/MainPageActivity.java
+++ app/src/main/java/com/example/vaiojun/dimension4/Activity/MainPageActivity.java
@@ -1,12 +1,16 @@
-package com.example.vaiojun.dimension4;
+package com.example.vaiojun.dimension4.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainPage extends AppCompatActivity {
+import com.example.vaiojun.dimension4.R;
+
+public class MainPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
